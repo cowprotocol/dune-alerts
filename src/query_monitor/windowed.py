baf91353efb84bf92451f7cdc61305783e4072f7
Ben Smith <bh2smith@gmail.com>
2022-07-29 09:23:24 +0200

    remove special runloop with window update in WindowedQueryMonitor

--- src/query_monitor/windowed.py
+++ src/query_monitor/windowed.py
@@ -6,9 +6,7 @@ import urllib.parse
 from datetime import datetime, timedelta
 from typing import Optional
 
-from duneapi.api import DuneAPI
 from duneapi.types import QueryParameter
-from slack.web.client import WebClient
 
 from src.models import TimeWindow
 from src.query_monitor.base import BaseQueryMonitor
@@ -55,19 +53,3 @@ class WindowedQueryMonitor(BaseQueryMonitor):
                 "window end time is beyond 2 hours in the past, some data may not yet be available"
             )
         self.window = window
-
-    def run_loop(
-        self, dune: DuneAPI, slack_client: WebClient, alert_channel: str
-    ) -> None:
-        """
-        Originally designed to run infinitely and shift the window after each successful run
-        If we go with a memory-free approach this will not be used.
-        However, it can be adjusted to use Dune as persistent storage as follows:
-            - Query can be loaded from dune by ID.
-            - query can then be updated with configuration parameters as new defaults
-                (i.e. make the defaults equal to the current run).
-            Then, on the next run, shift the time window from the last successful run.
-        """
-        super().run_loop(dune, slack_client, alert_channel)
-        # After a successful run loop, we update the window for the next
-        self._set_window(self.window.next())
