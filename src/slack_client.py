5df1c955ad564bb70506e7dec345dc1536cd6145
Ben Smith <bh2smith@gmail.com>
2022-11-10 11:56:56 +0100

    lint fix

--- src/slack_client.py
+++ src/slack_client.py
@@ -4,14 +4,15 @@ Since channel is fixed at the beginning and nobody wants to see unfurled media
 otherwise be unnecessarily repeated.
 """
 import ssl
-
-import certifi
 import logging.config
+import certifi
+
 from slack.errors import SlackApiError
 from slack.web.client import WebClient
 
 log = logging.getLogger(__name__)
 logging.config.fileConfig(fname="logging.conf", disable_existing_loggers=False)
+
 
 class BasicSlackClient:
     """
@@ -32,11 +33,11 @@ class BasicSlackClient:
         log.info(f"posting to slack channel: {self.channel}")
         try:
             self.client.chat_postMessage(
-                    channel=self.channel,
-                    text=message,
-                    # Do not show link preview!
-                    # https://api.slack.com/reference/messaging/link-unfurling
-                    unfurl_media=False,
-                )
+                channel=self.channel,
+                text=message,
+                # Do not show link preview!
+                # https://api.slack.com/reference/messaging/link-unfurling
+                unfurl_media=False,
+            )
         except SlackApiError as err:
             raise RuntimeError(f"slack post failed with {err}") from err
